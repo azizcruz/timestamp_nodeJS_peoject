b0921ac26860a933bbc60f9ddd9bc203364b96b5
Glitch (time-stamp-project-1) <none>
2018-04-15 20:08:52 +0000

    🌝🏩 Checkpoint ./server.js:903892/341 ./public/client.js:903892/318

--- server.js
+++ server.js
@@ -19,20 +19,10 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
+app.post("/date", (request, response) => {
+  var date = request.params.date;
+  console.log(date);
   response.sendStatus(200)
 })
 
